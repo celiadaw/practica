b6e34277230a1d48dd0e19671ea2ea3b651c4ea1
celiadaw <celiasalamancag@gmail.com>
2021-02-10 21:04:11 +0100

    cambios finales

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 
-prompt("Bienvenido");
-let answer= prompt ("¿Quieres multiplicar algún número? Si=1 / No=2");
-if (answer == 1){
+let yourName = prompt("¿Como te llamas?");
+
+function welcome(yourName) {
+ alert("Bienvenid@ "+ yourName);
+   
+ }
+
+ welcome(yourName);
+
+let answer = prompt ("¿Quieres multiplicar algún número? Si=1 / No=2");
+
+     
+do {
+if (answer == 1 ){
     let num1= prompt ("Escribe un numero");
     
     let num2 = prompt ("Dime otro numero");
     let result = num1 * num2;
-     prompt (result);
-     answer != 1;
-}else {
+   answer = prompt ("Este es el resultado " + result + " ¿si quieres volver a mutiplicar pulsa 1, para salir pulsa 2" );
+} 
 
-     prompt ("Ya puedes entrar en la página ");
+}while (answer ==1) ;
+
+ alert ("Ya puedes entrar en la página ");
 
 
-}
+
 
