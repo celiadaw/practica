78bb57695c28ed36e995faf7d51446f645e5d5e3
celiadaw <celiasalamancag@gmail.com>
2021-02-10 03:09:56 +0100

    modificado menu, pasado validator, inico js

---
+++ main.js
@@ -0,0 +1,17 @@
+
+prompt("Bienvenido");
+let answer= prompt ("¿Quieres multiplicar algún número? Si=1 / No=2");
+if (answer == 1){
+    let num1= prompt ("Escribe un numero");
+    
+    let num2 = prompt ("Dime otro numero");
+    let result = num1 * num2;
+     prompt (result);
+     answer != 1;
+}else {
+
+     prompt ("Ya puedes entrar en la página ");
+
+
+}
+
